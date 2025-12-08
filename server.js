2df51bd417da363e16aeffe5fcce3a3aacea037d
Vincent TWIZERE <v.twizere@alustudent.com>
2025-12-08 12:17:26 +0200

    updating servers.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 //creating express server
 const express= require('express');
 const dotenv = require('dotenv');
-const {db}=require('./config/db.js');
-const app =express();
+const {db} = require('./config/db.js');
+const app = express();
 //evn file config
 
 dotenv.config();
 //Test db conncetion
-db.getConnection((err)=>{
+db.getConnection((err) => {
     if(err){
         console.log('Database connection failed',err);
     }
@@ -17,11 +17,11 @@ db.getConnection((err)=>{
 });
 
 //routes and endpoints
-app.get('/',(req,res)=>{
+app.get('/',(req,res) => {
     res.send("Welcome to DSE Backend API developed by DSE Team");
 })
 //app listening port
-const PORT= process.env.PORT || 3000; 
-app.listen(PORT,()=>{
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
 });
