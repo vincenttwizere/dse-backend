28ba075a94d55b7fe0e3d9f3c9abf6adab9ba7a9
Vincent TWIZERE <v.twizere@alustudent.com>
2025-12-08 12:12:22 +0200

    adding autho codes

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,3 +28,40 @@ export const register = (req, res) => {
         );
     });
 };
+
+// login
+
+export const login = (req, res) =>{
+    const { email, password } = req.body;
+
+    //  check if the user exist in database
+
+  db.query("SELECT * FROM users WHERE email = ?", [email], (err, results) => {
+
+    if (err) return res.json({ err:  `Error occur ${err}` });
+
+    if(results.length === 0) return res.json({ message: "User not found" });
+  });
+
+//   compare  entered password  and the one in the database
+
+bcrypt.compare(password, results[0].password, (err, isMatch) => {
+
+    if (err) return res.json({ message: "Error comparing password" });
+    if (!isMatch) return res.json({ message: "Incorrect password" });
+
+    // create JWT token
+    const token = jwt.sign(
+        { id: results[0].id, email: results[0].email },
+        "your_jwt_secret_key",
+        { expiresIn: "1h" }
+    );
+
+    return res.json({
+        message: "Login successful",
+        token
+    });
+
+});
+
+}
